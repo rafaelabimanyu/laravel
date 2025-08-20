a753e68a740b5d98a82c315b52db9fdb9ea67f70
rafaelabimanyu <rafaelabimanyu333@gmail.com>
2025-08-21 01:00:50 +0700

    first materi by kenapa coding

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/siswa', function () {
+    return view('siswa.index');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
